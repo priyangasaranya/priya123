4afe29d17bd3e1eff6e82ed5f558c68e4d631743
priyangasaranya <34539230+priyangasaranya@users.noreply.github.com>
2019-02-19 14:45:37 +0530

    Create vowels.java

---
+++ vowels.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+class vowcons
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+char ch=sc.next.charAt(0);
+if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'||ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='u')
+{
+System.out.println("vowels");
+}
+else
+{
+System.out.println("consonent");
+}
+}
+}
