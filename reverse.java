ae99b5fd47ff978e2fd89733d117657a6d1caffe
priyangasaranya <34539230+priyangasaranya@users.noreply.github.com>
2019-08-07 18:37:03 -0700

    https://www.guvi.in/codekata-level?level=player&set=1#collapse1

---
+++ reverse.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class reverse
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+String s=sc.nextLine();
+StringBuffer sb=new StringBuffer(s);
+sb.reverse();
+System.out.print(sb);
